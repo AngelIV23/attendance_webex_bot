436566d2c4a467b2211bb400b8835b8c7aa0dfaf
Angel Inglese V <angel.inglese@gmail.com>
2022-08-13 05:38:49 +0000

    adding QR code feature

--- functions_attendance_webexbot.py
+++ functions_attendance_webexbot.py
@@ -1,6 +1,7 @@
 import requests
 import os, json
 from dotenv import load_dotenv
+from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 def load_dotenv_vars():
     load_dotenv()
@@ -38,7 +39,34 @@ def postWbx_roomMsg_md(access_token, room_id, message):
     body = { 'roomId': room_id, 'markdown': message }
     response = requests.post( url = apiUrl, json = body, headers = httpHeaders )
     return response.status_code
-### CODE BELOW THIS LINE CAN AND SHOULD BE DELETED
+
+def postFile_roomMsg(access_token, room_id, message, filename):
+    apiUrl = 'https://webexapis.com/v1/messages'
+    m = MultipartEncoder({'roomId': room_id,
+                      'text': message,
+                      'files': (filename, open(filename, 'rb'),'image/png')})
+    httpHeaders = { 'Content-Type': m.content_type, 'Authorization': 'Bearer ' + access_token }
+
+    # body = { 'roomId': room_id, 'text': message, 'files': [filename] }
+    response = requests.post( url = apiUrl, data = m, headers = httpHeaders )
+    return response
+
+def webex_me(access_token):
+    apiUrl = 'https://webexapis.com/v1/people/me'
+    httpHeaders = {'Authorization': 'Bearer ' + access_token,
+           'Content-type': 'application/json'}
+    # body = {'roomId': ROOM_ID, 'text': MESSAGE_TEXT}
+    response = requests.get(apiUrl, headers=httpHeaders)
+    if response.status_code == 200:
+        return response.text
+    else:
+        # Oops something went wrong...  Better do something about it.
+        print(response.status_code, response.text)
+        return f'Error {response.status_code} found during request'
+
+##################################################################
+################### CODE BELOW THIS LINE CAN AND SHOULD BE DELETED ONLY FOR TESTING PURPOSES ####################
+##################################################################
 
 bot_cred = load_dotenv_vars()
 # print(bot_cred)
@@ -52,7 +80,16 @@ webex_messages = [
     '[Danger, Will Robinson!!!](https://en.wikipedia.org/wiki/Lost_in_Space#Catchphrases)'
     ]
 room_id = json.loads(rooms_list.text)
+filename = 'qr-code-wifi.png'
 
 for msg in webex_messages:
     webex_user = 'angel.inglese@gmail.com'
-    crtl_response = postWbx_roomMsg_md(bot_cred[0], room_id['items'][0]['id'], msg)
+    postWbx_roomMsg_md(bot_cred[0], room_id['items'][0]['id'], msg)
+    
+
+crtl_message = postFile_roomMsg(bot_cred[0], room_id['items'][0]['id'], 'This is your QR code to connect to Corp SSID', filename)
+
+print(crtl_message.status_code)
+print(crtl_message.text)
+# bot_me = webex_me(bot_cred[0])
+# print(json.dumps(json.loads(bot_me), indent=4))
---
+++ ssid_qrcode.py
@@ -0,0 +1,59 @@
+import qrcode, os
+from PIL import Image
+
+def create_qrcode(qrcode_str):
+    for image in os.listdir('.'):
+         # If is png image
+         if image.endswith(".png"):
+            im = Image.open(image)
+            # and is not RGBA
+            if im.mode != 'RGBA':
+                im.convert("RGBA").save(f"rgb-{image}")
+
+    logo_link = 'rgb-cisco-logo.png'
+    logo = Image.open(logo_link)
+    
+    # taking base width
+    basewidth = 100
+    
+    # adjust image size
+    wpercent = (basewidth/float(logo.size[0]))
+    hsize = int((float(logo.size[1])*float(wpercent)))
+    logo = logo.resize((basewidth, hsize), Image.Resampling.BICUBIC)
+    QRcode = qrcode.QRCode(
+        error_correction=qrcode.constants.ERROR_CORRECT_H
+    )
+    
+    # taking url or text
+    qr_string = qrcode_str
+    
+    # adding URL or text to QRcode
+    QRcode.add_data(qr_string)
+    
+    # generating QR code
+    QRcode.make()
+    
+    # taking color name from user
+    QRcolor = 'Black'
+    
+    # adding color to QR code
+    QRimg = QRcode.make_image(
+        fill_color=QRcolor, back_color="white").convert('RGB')
+    
+    # set size of QR code
+    pos = ((QRimg.size[0] - logo.size[0]) // 2,
+        (QRimg.size[1] - logo.size[1]) // 2)
+    QRimg.paste(logo, pos)
+    
+    # save the QR code generated
+    QRimg.save(f'qr-code-wifi.png', quality = 95)
+
+
+
+ssid     = 'this_is_your_SSID'
+password = 'add_your_psk'
+auth_type = "WPA2"
+hidden = False
+wifi_qrcode_string = f"WIFI:T:{auth_type};S:{ssid};P:{password};H:{hidden};;"
+
+create_qrcode(wifi_qrcode_string)
